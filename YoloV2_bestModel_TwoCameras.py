0be99a02db6af114922a008c6a63c8733eebb1e3
Peter Mankowski <petermankowski510@gmail.com>
2020-05-11 14:12:13 -0400

    LED added
    RED LED added

--- YoloV2_bestModel_TwoCameras.py
+++ YoloV2_bestModel_TwoCameras.py
@@ -1,21 +1,40 @@
 #tested with frimware 5-0.22
-import sensor,image,lcd
+
+from Maix import GPIO
+from board import board_info
+from fpioa_manager import fm
+
+import sensor,image,lcd, utime, gc
 import KPU as kpu
 
+# register pin to gpiohs0,
+# arg force means force register no matter we have registered before or not
+# if arg force=False(by default), register func will return a tuple that registered info,
+#                                                           or return number 1
+fm.register(board_info.LED_R, fm.fpioa.GPIO0, force=True)
+fm.register(board_info.LED_G, fm.fpioa.GPIOHS0, force=True)
+#fm.register(board_info.BOOT_KEY, fm.fpioa.GPIO1, force=True)
+
+led_r = GPIO(GPIO.GPIO0, GPIO.OUT)
+led_g = GPIO(GPIO.GPIOHS0, GPIO.OUT)
+#input = GPIO(GPIO.GPIO1, GPIO.IN)
+
 lcd.init(freq=20000000)
+
+""" lcd orientation set-up"""
+lcd.direction(lcd.YX_RLUD)
+
 sensor.binocular_reset()
-#sensor.reset()
-#sensor.set_pixformat(sensor.RGB565)
+
+sensor.shutdown(False)
+sensor.set_pixformat(sensor.RGB565)
 #sensor.set_pixformat(sensor.GRAYSCALE)
 #sensor.set_pixformat(sensor.YUV422)
 #sensor.set_framesize(sensor.QVGA)
 
-
-
-sensor.shutdown(False)
-sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.set_windowing((224, 224))
+
 sensor.set_vflip(1)
 sensor.set_hmirror(0)
 
@@ -27,22 +46,14 @@ sensor.set_vflip(1)
 sensor.set_hmirror(0)
 sensor.run(1)
 
-
-
-
-
-
-#sensor.set_framesize(sensor.VGA)
-
-
-
-#sensor.run(1)
 classes=["aeroplane",  "bicycle", "bird",  "boat", "bottle","bus","car","cat","chair","cow","diningtable","dog",  "horse", "motorbike", "person","pottedplant","sheep",  "sofa",   "train",   "tvmonitor"]
 task = kpu.load("/sd/best_Y2.kmodel") #change to "/sd/name_of_the_model_file.kmodel" if loading from SD card
-#a = kpu.set_outputs(task, 0, 7,7,125) #the actual shape needs to match the last layer shape of your model(before Reshape)
+
 anchor = [0.078, 0.12988, 0.19478, 0.35306, 0.77938, 0.83391, 0.61065, 0.40658, 0.33872, 0.68793]
 
 a = kpu.init_yolo2(task, 0.35, 0.8, 5, anchor) #tweak the second parameter if you're getting too many false positives
+
+status = 0
 
 try:
  while(True):
@@ -51,15 +62,17 @@ try:
     #.rotation_corr(z_rotation=180.0)
     #a = img.pix_to_ai()
     code = kpu.run_yolo2(task, img)
+
     if code:
-        #print(code)
         for i in code:
-           #if i.classid()==11 or i.classid()==8 or i.classid()==10 or i.classid()==12:
            if i.classid()==1 or i.classid()==5 or i.classid()==6 or i.classid()==13:
             print(code)
-            a=img.draw_rectangle(i.rect(),color = (0, 255, 0),thickness=4)
+            status = 0 if (status==1) else 1
+            a=img.draw_rectangle(i.rect(),color = (0, 255, 0),thickness=10)
             a = img.draw_string(i.x(),i.y(), classes[i.classid()], color=(255,0,0), scale=3)
-            #image.cl
+            led_r.value(status)
+            led_g.value(status)
+
         img=img.resize(320,240)
         a = lcd.display(img)
 
@@ -69,26 +82,29 @@ try:
     sensor.shutdown(True)
     img = sensor.snapshot()
     #.rotation_corr(z_rotation=180.0)
-    #a = img.pix_to_ai()
     code = kpu.run_yolo2(task, img)
     if code:
-        #print(code)
+        led_r.value(0)
         for i in code:
-           #if i.classid()==11 or i.classid()==8 or i.classid()==10 or i.classid()==12:
            if i.classid()==1 or i.classid()==5 or i.classid()==6 or i.classid()==13:
             print(code)
-            a=img.draw_rectangle(i.rect(),color = (0, 255, 0),thickness=4)
+            a=img.draw_rectangle(i.rect(),color = (0, 255, 0),thickness=10)
             a = img.draw_string(i.x(),i.y(), classes[i.classid()], color=(255,0,0), scale=3)
-            #image.cl
         img=img.resize(320,240)
         a = lcd.display(img)
 
     else:
         img=img.resize(320,240)
         a = lcd.display(img)
-    #time.sleep_ms(100)
-except:
-    print("EXIT AAAAAAAAAAAAAAAAAA")
-    a = kpu.deinit(task)
 
-#a = kp
+except:
+    print("EXIT Two Camera Mode")
+    a = kpu.deinit(task)
+    led_r.value(1)
+    print("gc.mem_free collector enabled")
+    if(gc.mem_free()<600000):
+        gc.collect()
+
+    fm.unregister(board_info.LED_R, fm.fpioa.GPIO0)
+    fm.unregister(board_info.LED_G, fm.fpioa.GPIOHS0)
+    #fm.unregister(board_info.BOOT_KEY, fm.fpioa.GPIO1)
